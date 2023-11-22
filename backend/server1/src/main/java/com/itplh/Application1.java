251eb9bea7324a0b0748c95cdc6199058d22dc64
tanpenggood <tanpengswpu@163.com>
2023-11-22 09:43:29 +0800

    Add backend app1 app2

---
+++ backend/server1/src/main/java/com/itplh/Application1.java
@@ -0,0 +1,13 @@
+package com.itplh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application1.class, args);
+    }
+
+}
---
+++ backend/server1/src/main/java/com/itplh/controller/VersionController.java
@@ -0,0 +1,14 @@
+package com.itplh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @GetMapping("/version")
+    public String version() {
+        return "server1 1.0.0";
+    }
+
+}
