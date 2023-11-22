7b19b89d6979f9a6914660434ab5a5f6b310505c
tanpenggood <tanpengswpu@163.com>
2023-11-23 00:49:51 +0800

    [type: feat] (frontend/app1) Setup dynamic title VITE_APP_TITLE, setup alias @

--- frontend/app1/vite.config.js
+++ frontend/app1/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import path from 'path'
+
+// 为了在index.html中使用环境变量
+const getViteEnv = (mode, target) => {
+  return loadEnv(mode, process.cwd())[target];
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [
+      vue(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            //将环境变量 VITE_APP_TITLE 赋值给 title 方便 html页面使用 title 获取系统标题
+            title: getViteEnv(mode, "VITE_APP_TITLE"),
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, "./src")
+      }
+    },
+  })
